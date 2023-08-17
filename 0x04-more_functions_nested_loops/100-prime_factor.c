e4371bc1d8b843866cc11b625d77d4f858137f50
amir <lhm76726@gmail.com>
2023-08-17 12:41:29 +0100

    miaa

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,7 +3,8 @@
  * Author: amir-ee
  *
  * Description:
- * This program finds and prints the largest prime factor of the number 612852475143.
+ * This program finds and prints the largest prime factor of
+ * the number 612852475143.
  */
 
 #include <stdio.h>
@@ -19,17 +20,13 @@ int main(void)
 
 	/* Divide by 2 until prime is odd */
 	while (prime % 2 == 0)
-	{
 		prime /= 2;
-	}
 
 	/* Check odd divisors */
 	for (div = 3; div <= prime / 2; div += 2)
 	{
 		while (prime % div == 0)
-		{
 			prime /= div;
-		}
 	}
 
 	printf("%ld\n", prime);
