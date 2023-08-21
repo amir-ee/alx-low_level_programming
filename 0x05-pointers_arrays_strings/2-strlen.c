970d5bf45ca50dce5fd362cdf5e90f4063c15cb4
amir <lhm76726@gmail.com>
2023-08-21 23:07:14 +0100

    the z

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: String to evaluate.
+ * _strlen - Computes the length of a string.
+ * @s: The string to be evaluated.
  *
  * Return: The length of the string.
  */
@@ -10,7 +10,6 @@ int _strlen(char *s)
 {
 	int length = 0; /* Initialize the length */
 
-	/* Iterate through the string until the null terminator is reached */
 	while (s[length] != '\0')
 	{
 		length++; /* Increment the length */
