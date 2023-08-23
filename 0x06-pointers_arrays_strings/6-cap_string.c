eec1266cca346b989118fba3f26a50f58a706634
amir <lhm76726@gmail.com>
2023-08-23 18:55:54 +0100

    now z and a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * cap_string - Capitalizes the first letter of each word in a string.
- * @s: The input string.
- * Return: Pointer to the modified string.
+ * cap_string - Capitalize all words of a string.
+ *
+ * @s: Argument passed.
+ *
+ * Return: String pointer.
  */
 char *cap_string(char *s)
 {
 	int i, j;
-	char special_chars[] = " \t\n\r\v\f!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-	/* Capitalize the first letter if it's a lowercase alphabet */
-	if (s[0] >= 'a' && s[0] <= 'z')
-		s[0] -= 32;
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+		'!', '?', '"', '(', ')', '{', '}'};
 
-	/* Iterate through the string */
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		/* Check if the current character is a special character */
-		for (j = 0; special_chars[j] != '\0'; j++)
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+			s[i] -= 32;
+
+		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == special_chars[j])
+			if (s[i] == spe[j])
 			{
-				/* If the next character is a lowercase alphabet, capitalize it */
 				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
 				{
 					s[i + 1] -= 32;
@@ -30,6 +30,6 @@ char *cap_string(char *s)
 			}
 		}
 	}
+
 	return (s);
 }
-
