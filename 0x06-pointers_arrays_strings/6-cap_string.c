a245ba196b562d18c4fc93751a49093bcfb2fb10
amir <lhm76726@gmail.com>
2023-08-23 18:50:57 +0100

    now z and a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,29 +7,29 @@
  */
 char *cap_string(char *s)
 {
-    int i, j;
-    char special_chars[] = " \t\n\r\v\f!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+	int i, j;
+	char special_chars[] = " \t\n\r\v\f!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-    // Capitalize the first letter if it's a lowercase alphabet
-    if (s[0] >= 'a' && s[0] <= 'z')
-        s[0] -= 32;
+	/* Capitalize the first letter if it's a lowercase alphabet */
+	if (s[0] >= 'a' && s[0] <= 'z')
+		s[0] -= 32;
 
-    // Iterate through the string
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        // Check if the current character is a special character
-        for (j = 0; special_chars[j] != '\0'; j++)
-        {
-            if (s[i] == special_chars[j])
-            {
-                // If the next character is a lowercase alphabet, capitalize it
-                if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-                {
-                    s[i + 1] -= 32;
-                }
-            }
-        }
-    }
-    return s;
+	/* Iterate through the string */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		/* Check if the current character is a special character */
+		for (j = 0; special_chars[j] != '\0'; j++)
+		{
+			if (s[i] == special_chars[j])
+			{
+				/* If the next character is a lowercase alphabet, capitalize it */
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
 }
 
