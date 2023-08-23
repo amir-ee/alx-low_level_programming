9bce80c18becf69e56b0a2dc8ab904b4519f8f0c
amir <lhm76726@gmail.com>
2023-08-23 19:24:49 +0100

    now z and a

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,84 +1,63 @@
 #include "main.h"
 
 /**
- * rev_string - Reverses an array of characters.
- * @s: Pointer to the string to be reversed.
- *
- * Return: void.
+ * rev_string - reverse array
+ * @s: string to be reversed
+ * Return: void
  */
 void rev_string(char *s)
 {
 	int start = 0;
-	int end = 0;
+	int end = strlen(s) - 1;
 	char temp;
 
-	/* Find the length of the string */
-	while (*(s + end) != '\0')
+	while (start < end)
 	{
-		end++;
-	}
-	end--;
-
-	/* Swap characters to reverse the string */
-	for (start = 0; start < end; start++, end--)
-	{
-		temp = *(s + start);
-		*(s + start) = *(s + end);
-		*(s + end) = temp;
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
 	}
 }
 
 /**
- * infinite_add - Adds two numbers together.
- * @n1: Text representation of the first number to add.
- * @n2: Text representation of the second number to add.
- * @r: Pointer to the result buffer.
- * @size_r: Buffer size.
- *
- * Return: Pointer to the result string.
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of the first number to add
+ * @n2: text representation of the second number to add
+ * @r: pointer to the result buffer
+ * @size_r: buffer size
+ * Return: pointer to the result string
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int carry = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_sum = 0;
+	int overflow = 0, first_number_index = strlen(n1) - 1,
+		second_number_index = strlen(n2) - 1,
+		result_string_length = 0;
+	int val1, val2, temp_tot;
 
-	/* Find lengths of n1 and n2 */
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-
-	/* Check if result buffer size is sufficient */
-	if (j >= size_r || i >= size_r)
-		return (0);
-
-	/* Perform addition with overflow handling */
-	while (j >= 0 || i >= 0 || carry == 1)
+	while (first_number_index >= 0 || second_number_index >= 0 || overflow == 1)
 	{
-		val1 = (i >= 0) ? *(n1 + i) - '0' : 0;
-		val2 = (j >= 0) ? *(n2 + j) - '0' : 0;
-
-		temp_sum = val1 + val2 + carry;
-
-		carry = temp_sum / 10;
-
-		if (digits >= (size_r - 1))
-			return (0);
-
-		*(r + digits) = (temp_sum % 10) + '0';
-		digits++;
-		j--;
-		i--;
+		if (first_number_index < 0)
+			val1 = 0;
+		else
+			val1 = n1[first_number_index] - '0';
+		if (second_number_index < 0)
+			val2 = 0;
+		else
+			val2 = n2[second_number_index] - '0';
+		temp_tot = val1 + val2 + overflow;
+		overflow = temp_tot >= 10;
+		if (result_string_length >= size_r - 1)
+			return (NULL);
+		r[result_string_length++] = temp_tot % 10 + '0';
+		first_number_index--;
+		second_number_index--;
 	}
-
-	if (digits == size_r)
-		return (0);
-
-	*(r + digits) = '\0';
+	if (result_string_length == size_r)
+		return (NULL);
+	r[result_string_length] = '\0';
 	rev_string(r);
-
 	return (r);
 }
 
