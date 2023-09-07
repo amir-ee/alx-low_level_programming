9d970f2d8c5ce106d8cac1eb069ee02312033f7c
amir <lhm76726@gmail.com>
2023-09-07 13:07:53 +0100

    UI

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,98 +3,99 @@
 #include "main.h"
 
 /**
- * is_digit - checks if a string contains only digit characters
- * @s: The string to be evaluated
+ * is_digit - checks if a string contains a non-digit char
  *
- * Return: 0 if a non-digit character is found, 1 otherwise
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
 {
-    int index = 0;
+	int i = 0;
 
-    while (s[index])
-    {
-        if (s[index] < '0' || s[index] > '9')
-            return (0);
-        index++;
-    }
-    return (1);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
- * str_length - returns the length of a string
- * @s: The string to evaluate
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: The length of the string
+ * Return: the length of the string
  */
-int str_length(char *s)
+int _strlen(char *s)
 {
-    int length = 0;
+	int i = 0;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
-    return (length);
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
 /**
- * handle_errors - handles errors for the main function
+ * invalidArgs - handles errors of the main
  */
-void handle_errors(void)
+void invalidArgs(void)
 {
-    printf("Error\n");
-    exit(98);
+	printf("Error\n");
+	exit(98);
 }
 
 /**
- * main - multiplies two positive numbers represented as strings
- * @argc: The number of command-line arguments
- * @argv: An array of command-line argument strings
+ * main - multiplies two positive numbers
  *
- * Return: Always returns 0 (Success)
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-    char *num_str1, *num_str2;
-    int len1, len2, total_len, i, carry, digit1, digit2, *result, has_non_zero = 0;
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-    num_str1 = argv[1], num_str2 = argv[2];
-    if (argc != 3 || !is_digit(num_str1) || !is_digit(num_str2))
-        handle_errors();
-    len1 = str_length(num_str1);
-    len2 = str_length(num_str2);
-    total_len = len1 + len2 + 1;
-    result = malloc(sizeof(int) * total_len);
-    if (!result)
-        return (1);
-    for (i = 0; i <= len1 + len2; i++)
-        result[i] = 0;
-    for (len1 = len1 - 1; len1 >= 0; len1--)
-    {
-        digit1 = num_str1[len1] - '0';
-        carry = 0;
-        for (len2 = str_length(num_str2) - 1; len2 >= 0; len2--)
-        {
-            digit2 = num_str2[len2] - '0';
-            carry += result[len1 + len2 + 1] + (digit1 * digit2);
-            result[len1 + len2 + 1] = carry % 10;
-            carry /= 10;
-        }
-        if (carry > 0)
-            result[len1 + len2 + 1] += carry;
-    }
-    for (i = 0; i < total_len - 1; i++)
-    {
-        if (result[i])
-            has_non_zero = 1;
-        if (has_non_zero)
-            putchar(result[i] + '0');
-    }
-    if (!has_non_zero)
-        putchar('0');
-    putchar('\n');
-    free(result);
-    return (0);
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		invalidArgs();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i < len; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
-
