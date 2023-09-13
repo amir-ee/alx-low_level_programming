98a2d0526cbdbf9f2dcba36288e00c9061037939
amir <lhm76726@gmail.com>
2023-09-13 15:31:51 +0100

    try_hck_me

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Prints a name using a given function pointer
- * @name: Pointer to a character array containing the name to print
- * @f: Pointer to a function that takes a pointer to a character array
- *  and returns no value
+ * print_name - prints a name using a given function pointer
+ * @name: pointer to a character array containing the name to print
+ * @f: pointer to a function that takes a pointer to a character array
+ *     and returns no value
  *
- * Description:
  * This function prints the specified name using the function pointed to by f.
- * If either the name or f pointers are NULL, the function returns without printing.
+ * If either the name or
+ * f pointers are NULL, the function returns without printing.
  */
 
 void print_name(char *name, void (*f)(char *))
