60caf5bd7218d73e3dd969b8441b4e190470b936
amir <lhm76726@gmail.com>
2023-09-26 23:39:45 +0100

    null

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,42 +1,46 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes a node at a specific index in a listint_t list
- * @head: Pointer to the pointer to the head of the list
- * @index: The index of the node to delete
- *
- * Description:
- * This function deletes the node at the specified index in a listint_t list.
- * If the index is out of range or the list is empty, it returns -1. Otherwise,
- * it returns 1 after successfully deleting the node.
+ * delete_nodeint_at_index - Deletes a node at a specific index in a list.
+ * @head: Pointer to the pointer to the list's head.
+ * @index: Index of the node to delete.
  *
  * Return: 1 on success, -1 on failure.
  */
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-  listint_t *tmp, *copy = *head;
-  unsigned int node;
+unsigned int i;
+listint_t *prev;
+listint_t *next;
 
-  if (copy == NULL)
-    return (-1);
+prev = *head;
 
-  if (index == 0)
-    {
-      *head = (*head)->next;
-      free(copy);
-      return (1);
-    }
+if (index != 0)
+{
+for (i = 0; i < index - 1 && prev != NULL; i++)
+{
+prev = prev->next;
+}
+}
 
-  for (node = 0; node < (index - 1); node++)
-    {
-      if (copy->next == NULL)
-	return (-1);
+if (prev == NULL || (prev->next == NULL && index != 0))
+{
+return (-1);
+}
 
-      copy = copy->next;
-    }
+next = prev->next;
 
-  tmp = copy->next;
-  copy->next = tmp->next;
-  free(tmp);
-  return (1);
+if (index != 0)
+{
+prev->next = next->next;
+free(next);
+}
+else
+{
+free(prev);
+*head = next;
+}
+
+return (1);
 }
