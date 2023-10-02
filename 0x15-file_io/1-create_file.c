4316969aea4207d94583622948e7a6db8ac3fe4d
amir <lhm76726@gmail.com>
2023-10-02 14:19:14 +0100

    file io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,21 +15,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd = 0, wr = 0, len = 0;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 	if (fd < 0)
-		fd = open(filename, O_WRONLY | O_CREAT, 0600);
-	if (fd >= 0)
+		return (-1);
+	if (text_content && *text_content)
 	{
-		for (i = 0; text_content != NULL && *(text_content + i) != '\0'; i++)
+		while (text_content[len++])
+			;
+		wr = write(fd, text_content, len - 1);
+		if (wr < 0)
 		{
-			if (write(fd, text_content + i, 1) != 1)
-				return (-1);
+			close(fd);
+			return (-1);
 		}
-		close(fd);
 	}
+	close(fd);
 	return (1);
 }
