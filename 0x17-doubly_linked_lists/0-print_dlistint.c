3e0457aebd83b5802ad99a3c5f8cd91584b1c30c
amir <lhm76726@gmail.com>
2023-11-09 19:03:07 +0100

    okaoka->okh = yeah

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,10 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Print the elements of a doubly linked list
+ * print_dlistint - Prints the elements of a doubly linked list
+ * @h: Pointer to the head of the list
  *
- * This function print the elements of a db linked list starting from the given
- * node and moving forward. It also counts the number of elements in the list.
- *
- * @h: A pointer to the head of the doubly linked list.
- *
- * Return:
- * The number of elements in the list (the size of the list).
+ * Return: The number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
